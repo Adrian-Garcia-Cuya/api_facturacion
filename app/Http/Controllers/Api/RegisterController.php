9f3adc52ebaa8c54a3e65de6f362dec09339a3dc
Adrian Garcia <adrianisa123575@outlook.com>
2024-04-28 21:42:29 -0500

    First commit

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class RegisterController extends Controller
+{   
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $request['password'] = bcrypt($request['password']);
+        $user = User::create($request->all());
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => JWTAuth::factory()->getTTL(),
+        ]);
+    }
+}
